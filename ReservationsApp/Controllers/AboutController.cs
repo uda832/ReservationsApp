ef918310131059106aa853726f8858680f1ca745
Uda <oyeruultseng@mail.usf.edu>
2020-03-07 23:16:24 -0500

    Controllers

---
+++ ReservationsApp/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReservationsApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+
+        public ActionResult Get()
+        {
+            return Ok(new { 
+                Description = "API is the service for the ReservationApp",
+                AssemblyVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(),
+            });
+        }
+    }
+}
---
+++ ReservationsApp/Controllers/ReservationController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReservationsApp.Data;
+using ReservationsApp.Models;
+
+namespace ReservationsApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly ReservationContext _context;
+
+        public ReservationController(ReservationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Reservation
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Reservation>>> GetReservations()
+        {
+            return await _context.Reservations.ToListAsync();
+        }
+
+        // GET: api/Reservation/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Reservation>> GetReservationById(int id)
+        {
+            var reservation = await _context.Reservations.FindAsync(id);
+
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return reservation;
+        }
+
+        // PUT: api/Reservation/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReservation(int id, Reservation reservation)
+        {
+            if (id != reservation.ReservationID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(reservation).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReservationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Reservation
+        [HttpPost]
+        public async Task<ActionResult<Reservation>> PostReservation(Reservation reservation)
+        {
+            _context.Reservations.Add(reservation);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetReservationById", new { id = reservation.ReservationID }, reservation);
+        }
+
+        // DELETE: api/Reservation/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Reservation>> DeleteReservation(int id)
+        {
+            var reservation = await _context.Reservations.FindAsync(id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            _context.Reservations.Remove(reservation);
+            await _context.SaveChangesAsync();
+
+            return reservation;
+        }
+
+        private bool ReservationExists(int id)
+        {
+            return _context.Reservations.Any(e => e.ReservationID == id);
+        }
+    }
+}
