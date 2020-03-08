da2191fb720b81b76fc60ba356918810046e0fc2
Uda <oyeruultseng@mail.usf.edu>
2020-03-08 14:15:35 -0400

    HealthCheck controller

---
+++ ReservationsAPI/Controllers/HealthCheckController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReservationsAPI.Utilities;
+
+namespace ReservationsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            string status = HealthCheckUtility.GetStatus();
+
+            return (status == "offline")
+                ? StatusCode(StatusCodes.Status503ServiceUnavailable, new 
+                    {
+                        Status = StatusCodes.Status503ServiceUnavailable,
+                        Message = "service is offline",
+                    })
+                : Ok(status);
+        }
+    }
+}
---
+++ ReservationsAPI/Utilities/HealthCheckUtility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationsAPI.Utilities
+{
+    public static class HealthCheckUtility
+    {
+        private const string LOAD_BALANCER_PATH_ROOT = "C:\\AppHealthCheck";
+
+        public static string GetStatus()
+        {
+            string status = "";
+            string assemblyName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+            string filePath = LOAD_BALANCER_PATH_ROOT + "\\" + assemblyName + ".offline.txt";
+
+            status = System.IO.File.Exists(filePath) 
+                ? "offline" 
+                : assemblyName + " is online at " + DateTime.UtcNow.ToString();
+
+            return status;
+        }
+    }
+}
